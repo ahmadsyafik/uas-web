29646047342c054a6b27174a03dfb924a85affb0
ahmadsyafik <amirsyaifuddin0165@gmail.com>
2025-05-27 13:06:30 +0700

    tes

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,14 @@ import mysql from "mysql2/promise"
 
 // Create a connection pool to the MySQL database
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "mysql.railway.internal",
   user: "root",
-  password: "",
-  database: "cinedark",
+  password: "yOEJquRfLYECUMoKXvZQemLZAfiVmqNp",
+  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  port : 3306,
 })
 
 export default pool
